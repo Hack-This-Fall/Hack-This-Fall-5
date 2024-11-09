a4da85c9baa9bef7db2094ffccd03b552cd424c4
Apoorv Dwivedi <apoorvd14@gmail.com>
2024-11-09 21:51:25 +0530

    updated meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,56 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
-import { Roboto_Flex, Roboto_Condensed, Outfit } from "next/font/google";
-import { Providers } from "./providers";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
+import { Roboto_Flex, Roboto_Condensed, Outfit } from 'next/font/google';
+import { Providers } from './providers';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const robotoFlex = Roboto_Flex({
-  subsets: ["latin"],
-  variable: "--font-roboto-flex",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ['latin'],
+  variable: '--font-roboto-flex',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 const robotoCondensed = Roboto_Condensed({
-  subsets: ["latin"],
-  variable: "--font-roboto-condensed",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ['latin'],
+  variable: '--font-roboto-condensed',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 const outfit = Outfit({
-  subsets: ["latin"],
-  variable: "--font-outfit",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ['latin'],
+  variable: '--font-outfit',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "Hack This Fall Virtual Hackathon 2024",
+  title: 'Hack This Fall In-Person Hackathon 2025',
   description:
-    "Join us from anywhere in the world and be part of this epic experience. Whether you’re already in our global community or new to the scene, we welcome you with open arms! 🗓️ 8 - 10 NOV 2024.",
+    "Join us at our in-person hackathon and be part of this epic experience. Whether you're already part of our community or new to the scene, we welcome you with open arms! 🗓️ 21 - 23 March 2025.",
   openGraph: {
-    title: "Hack This Fall Virtual Hackathon 2024",
+    title: 'Hack This Fall In-Person Hackathon 2025',
     description:
-      "Join us from anywhere in the world and be part of this epic experience. Whether you’re already in our global community or new to the scene, we welcome you with open arms! 🗓️ 8 - 10 NOV 2024.",
-    siteName: "Hack This Fall Virtual Hackathon 2024",
+      "Join us at our in-person hackathon and be part of this epic experience. Whether you're already part of our community or new to the scene, we welcome you with open arms! 🗓️ 21 - 23 March 2025.",
+    siteName: 'Hack This Fall In-Person Hackathon 2025',
     images: [
       {
-        url: "https://hackathon.hackthisfall.tech/og.png",
+        url: 'https://hackathon.hackthisfall.tech/banner.png',
         width: 2000,
         height: 1000,
-        alt: "Hack This Fall Virtual Hackathon 2024",
+        alt: 'Hack This Fall In-Person Hackathon 2025',
       },
     ],
   },
   twitter: {
-    title: "Hack This Fall Virtual Hackathon 2024",
+    title: 'Hack This Fall In-Person Hackathon 2025',
     description:
-      "Join us from anywhere in the world and be part of this epic experience. Whether you’re already in our global community or new to the scene, we welcome you with open arms! 🗓️ 8 - 10 NOV 2024.",
+      "Join us at our in-person hackathon and be part of this epic experience. Whether you're already part of our community or new to the scene, we welcome you with open arms! 🗓️ 21 - 23 March 2025.",
     images: [
       {
-        url: "https://hackathon.hackthisfall.tech/og.png",
+        url: 'https://hackathon.hackthisfall.tech/banner.png',
         width: 2000,
         height: 1000,
-        alt: "Hack This Fall Virtual Hackathon 2024",
+        alt: 'Hack This Fall In-Person Hackathon 2025',
       },
     ],
   },
